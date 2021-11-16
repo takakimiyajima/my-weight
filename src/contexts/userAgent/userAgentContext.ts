9798bf3459df4bf7829c6eaafc80b0e7c9a4ab6c
Miyajima Takaki <takaki.mz@gmail.com>
2021-11-16 08:12:49 -0800

    remove: semicolon

--- src/contexts/userAgent/userAgentContext.ts
+++ src/contexts/userAgent/userAgentContext.ts
@@ -1,5 +1,5 @@
-import { createContext } from "react"
-import { UserViewStatus } from "./useUserAgent"
+import { createContext } from 'react'
+import { UserViewStatus } from './useUserAgent'
 
 export const UserAgentContext = createContext<UserViewStatus>({
   isMobile: false,
