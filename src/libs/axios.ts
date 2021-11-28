13e5f852df6b1588ceac8de445e530d28655a97a
Miyajima Takaki <takaki.mz@gmail.com>
2021-11-28 05:36:45 -0800

    remove: env axios

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -4,7 +4,6 @@ import axiosBase from 'axios'
  * PickgoのRubyサーバと通信するためのaxiosインスタンス
  */
 const axios = axiosBase.create({
-  baseURL: process.env.ENDPOINT,
   headers: {
     /** FIXME: fix token for POST */
     'X-MICROCMS-API-KEY': process.env.API_KEY ?? '879978592c79415e8d7c161468cbd5767909',
