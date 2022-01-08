0afb01e5ae7618ea135b61fa38acd5e80033631a
Miyajima Takaki <takaki.mz@gmail.com>
2022-01-08 13:07:02 -0800

    restore: token for now

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -3,7 +3,8 @@ import axiosBase from 'axios'
 const axios = axiosBase.create({
   baseURL: 'https://takakimiyajima.microcms.io/api/v1',
   headers: {
-    'X-MICROCMS-API-KEY': process.env.API_KEY ?? '',
+    /** FIXME: fix token for POST */
+    'X-MICROCMS-API-KEY': process.env.API_KEY ?? '879978592c79415e8d7c161468cbd5767909',
     'Content-Type': 'application/json'
    }
 })
